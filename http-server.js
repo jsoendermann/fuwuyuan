319d22a73b82761dcb9d022f45600e1db91246ff
Jan Soendermann <jan.soendermann+git@gmail.com>
2017-06-15 11:25:11 +0800

    Fix bugs

--- http-server.js
+++ http-server.js
@@ -2,16 +2,15 @@ const http = require('http')
 
 
 const server = http.createServer((req, res) => {
-  console.log(JSON.stringify(req.headers))
   if (req.headers['x-github-event'] === 'push') {
     let body = ''
     req.on('data', d => { body += d })
     req.on('end', () => {
       let data
-      console.log(`DEBUG ${data}`)
       try {
-        const data = JSON.parse(body)
+        data = JSON.parse(body)
       } catch (e) { console.error(e.message) }
+      console.log(`DEBUG ${data}`)
       if (data.ref.startsWith('refs/heads/')) {
         const repositoryName = data.repository.full_name
         const branch = data.ref.slice('refs/heads/'.length)
